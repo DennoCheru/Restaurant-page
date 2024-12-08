4927404a67bbbdc05e48b69d46082c8292b4f9c6
Cheru <dennocheruh@gmail.com>
2024-12-08 13:36:29 +0300

    Add clearContent function
    Added a function to clear content on button click

--- src/index.js
+++ src/index.js
@@ -5,10 +5,24 @@ import '../src/styles.css';
 
 console.log('Welcome to our restaurant');
 
+function clearContent() {
+    const content = document.getElementById('content')
+    content.textContent = '';
+}
+
 function init() {
-    document.getElementById('homeBtn').addEventListener('click', createHomePage);
-    document.getElementById('menuBtn').addEventListener('click', createMenuPage);
-    document.getElementById('contactBtn').addEventListener('click', createContactPage);
+    document.getElementById('homeBtn').addEventListener('click', () => {
+        clearContent();
+        createHomePage();
+    });
+    document.getElementById('menuBtn').addEventListener('click', () => {
+        clearContent();
+        createMenuPage();
+    });
+    document.getElementById('contactBtn').addEventListener('click', () => {
+        clearContent();
+        createContactPage();
+    });
     createHomePage()
 }
 
