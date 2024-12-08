ff0947116f9967e2f8ecee729a6e8720aef3b184
Cheru <dennocheruh@gmail.com>
2024-12-08 14:14:47 +0300

    Fix navbar, highlight active page
    Added a fixed nav bar, highlighted the active page
    on the navbar

--- src/index.js
+++ src/index.js
@@ -10,19 +10,31 @@ function clearContent() {
     content.textContent = '';
 }
 
+function setActiveBtn(buttonId) {
+    const buttons = document.querySelectorAll('nav button');
+    buttons.forEach(button => {
+        button.classList.remove('active');
+    });
+    document.getElementById(buttonId).classList.add('active');
+}
+
 function init() {
     document.getElementById('homeBtn').addEventListener('click', () => {
         clearContent();
+        setActiveBtn('homeBtn');
         createHomePage();
     });
     document.getElementById('menuBtn').addEventListener('click', () => {
         clearContent();
+        setActiveBtn('menuBtn');
         createMenuPage();
     });
     document.getElementById('contactBtn').addEventListener('click', () => {
         clearContent();
+        setActiveBtn('contactBtn');
         createContactPage();
     });
+    setActiveBtn('homeBtn');
     createHomePage()
 }
 
